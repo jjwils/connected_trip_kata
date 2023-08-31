66c8ae8ae43e4dbba5bbd6975d34f38db95ae5d3
jjwils <mail@johnwilson.me.uk>
2023-08-31 21:35:20 +0100

    refactor

--- src/main/java/Taxis.java
+++ src/main/java/Taxis.java
@@ -3,55 +3,74 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Taxis {
-    private final List<Taxi> taxiList;
+    private final List<Taxi> taxis;
 
     public Taxis(List<Taxi> taxiList) {
 
-        this.taxiList = taxiList;
+        this.taxis = taxiList;
     }
 
     public void move(String command) {
 
-        Taxi topTaxi = getTopTaxi(taxiList);
-
-        int gridsInUse = numberOfGridsInUse(taxiList);
-
-        if (gridsInUse == 3) {
-            taxiList.forEach(taxi -> taxi.actuallyMove(command));
+        if (allTaxisInDifferentGrids()) {
+            moveAllTaxis(command);
         }
 
         else {
 
-            //lets keep a handle on where the top taxi currently is
-            String topTaxiGridref = topTaxi.gridref();
+            //lets keep a handle on where the highest priority taxi currently is
+            String highestPriorityTaxiGridref = highestPriorityTaxi().gridref();
 
             //top taxi always gets to move regardless
-            topTaxi.actuallyMove(command);
-            taxiList.remove(topTaxi);
+            highestPriorityTaxi().actuallyMove(command);
 
             //with the top taxi no longer counted then if we have two everyone can move
             //unless they were in the same grid as the top taxi
-            gridsInUse = numberOfGridsInUse(taxiList);
+            taxis.remove(highestPriorityTaxi());
 
-            if (gridsInUse == 2){
-                taxiList.forEach(taxi -> {
-                    if (!taxi.gridref().equals(topTaxiGridref))
-                    taxi.actuallyMove(command);
-
-                });
+            if (allTaxisInDifferentGrids()){
+                moveEachTaxiIfNotInSameGridAsHigherPriorityTaxi(command, highestPriorityTaxiGridref);
             }
-            //otherwise the remaining two must be in the same grid so only allow the new toptaxi to move
-            //again only if they were not in the same grid as the original top taxi
-            else{
-                Taxi newTopTaxi = getTopTaxi(taxiList);
 
-                if (!newTopTaxi.gridref().equals(topTaxiGridref))
+            //otherwise the remaining two must be in the same grid so only allow the new highest priority
+            // taxi to move again only if they were not in the same grid as the original top taxi
+            else{
+
+                if (newHighestPriorityTaxiNotInSameGridAsPreviousHighestPriorityTaxi(highestPriorityTaxiGridref))
                 {
-                    newTopTaxi.actuallyMove(command);
+                    highestPriorityTaxi().actuallyMove(command);
                 }
             }
 
         }
+    }
+
+    private Taxi highestPriorityTaxi() {
+        return highestPriorityTaxi(taxis);
+    }
+
+    private void moveAllTaxis(String command) {
+        taxis.forEach(taxi -> taxi.actuallyMove(command));
+    }
+
+    private void moveEachTaxiIfNotInSameGridAsHigherPriorityTaxi(String command, String highestPriorityTaxiGridref) {
+        taxis.forEach(taxi -> {
+            if (!taxi.gridref().equals(highestPriorityTaxiGridref))
+            taxi.actuallyMove(command);
+
+        });
+    }
+
+    private boolean newHighestPriorityTaxiNotInSameGridAsPreviousHighestPriorityTaxi(String highestPriorityTaxiGridref) {
+        return !highestPriorityTaxi().gridref().equals(highestPriorityTaxiGridref);
+    }
+
+    private boolean allTaxisInDifferentGrids() {
+        return taxisInDifferentGrids(numberOfGridsInUse(taxis));
+    }
+
+    private boolean taxisInDifferentGrids(int gridsInUse) {
+        return gridsInUse == taxis.size();
     }
 
     private int numberOfGridsInUse(List<Taxi> taxis) {
@@ -59,13 +78,13 @@ public class Taxis {
                 .size();
     }
 
-    private Taxi getTopTaxi(List<Taxi> taxis) {
+    private Taxi highestPriorityTaxi(List<Taxi> taxis) {
         return taxis.stream()
                 .min(Comparator.comparing(taxi -> taxi.getTaxiName()))
                 .orElse(null);
     }
 
     public List<Taxi> taxis() {
-        return taxiList;
+        return taxis;
     }
 }
